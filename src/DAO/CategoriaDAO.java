3d77e20a632339b1cb6c7069e9af03fe2c331c0d
Carlosg66 <guevaracarloseduardo66@gmail.com>
2025-04-08 21:00:24 +0200

    Se crearon todos los DAO exitosamente

--- src/DAO/CategoriaDAO.java
+++ src/DAO/CategoriaDAO.java
@@ -8,6 +8,9 @@ import Util.ConexionDB;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author yuri guevara
@@ -25,19 +28,41 @@ public class CategoriaDAO {
     }
     
     
+        // Método para leer todas las categorías
+    public List<Categoria> leerTodasCategorias() throws SQLException {
+    String sql = "SELECT * FROM Categorias";
+    List<Categoria> categorias = new ArrayList<>();
+    try (Connection c = ConexionDB.getConnection();
+        PreparedStatement stmt = c.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery()) {
+        while (rs.next()) {
+            Categoria categoria = new Categoria();
+            categoria.setIdCategoria(rs.getInt("id_categoria"));
+            categoria.setNombreCategoria(rs.getString("nombre_categoria"));
+            categoria.setDescripcionCategoria(rs.getString("descripcion_categoria"));
+            categorias.add(categoria);
+            }
+        }
+        return categorias;
+    }
+    
     // Método temporal para probar
     public static void main(String[] args) {
         try {
             CategoriaDAO dao = new CategoriaDAO();
-            Categoria c1 = new Categoria();
-            c1.setNombreCategoria("Herramientas");
-            c1.setDescripcionCategoria("Categoría para herramientas de ferretería");
-            dao.crearCategoria(c1);
-            System.out.println("Categoría creada con éxito!");
-    } catch (SQLException e) {
-        System.err.println("Error al crear categoría: " + e.getMessage());
+            // Leer y mostrar todas las categorías
+            List<Categoria> categorias = dao.leerTodasCategorias();
+            System.out.println("\nLista de categorías:");
+            for (Categoria cat : categorias) {
+                System.out.println("ID: " + cat.getIdCategoria()
+                        + ", Nombre: " + cat.getNombreCategoria()
+                        + ", Descripción: " + cat.getDescripcionCategoria());
+
+            }
+        } catch (SQLException e) {
+            System.err.println("Error al crear categoría: " + e.getMessage());
+        }
     }
-}
 
 
 }
--- src/Modelo/Compra.java
+++ src/Modelo/Compra.java
@@ -12,20 +12,20 @@ import java.util.List;
  */
 public class Compra {
     private int idCompra;
-    private Empleado empleado; // Relación con Empleado
+    private int idEmpleado; // Relación con Empleado
     private Date fechaCompra;
     private float totalCompra;
     private List<DetalleCompra> detalles; // Relación con DetalleCompra
 
-    public Compra(int idCompra, Empleado empleado, Date fechaCompra, float totalCompra, List<DetalleCompra> detalles) {
+    public Compra() {
+    }
+
+    public Compra(int idCompra, int idEmpleado, Date fechaCompra, float totalCompra, List<DetalleCompra> detalles) {
         this.idCompra = idCompra;
-        this.empleado = empleado;
+        this.idEmpleado = idEmpleado;
         this.fechaCompra = fechaCompra;
         this.totalCompra = totalCompra;
         this.detalles = detalles;
-    }
-
-    public Compra() {
     }
 
     public int getIdCompra() {
@@ -36,12 +36,12 @@ public class Compra {
         this.idCompra = idCompra;
     }
 
-    public Empleado getEmpleado() {
-        return empleado;
+    public int getIdEmpleado() {
+        return idEmpleado;
     }
 
-    public void setEmpleado(Empleado empleado) {
-        this.empleado = empleado;
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
     }
 
     public Date getFechaCompra() {
@@ -67,7 +67,7 @@ public class Compra {
     public void setDetalles(List<DetalleCompra> detalles) {
         this.detalles = detalles;
     }
-    
+
     
     
 }
